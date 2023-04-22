b2027d0cf47fcdd6475975d85dbf3a474ff034a8
NagesoHaile <nagesoh21@gmail.com>
2023-04-22 10:32:45 +0300

    djoser app added. authentication settings and urls configured

--- flowpy/settings.py
+++ flowpy/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     #third-party
     'rest_framework',
-    
+    'djoser',
     #local
     'core',
 
@@ -128,5 +128,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+      'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES':('JWT',)
+}
 
 AUTH_USER_MODEL = 'core.User'
